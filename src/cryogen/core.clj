f91c5567252ee55b071675271a5ae1690a62384a
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-07-21 16:45:29 +0100

    first commit

---
+++ src/cryogen/core.clj
@@ -0,0 +1,1 @@
+(ns cryogen.core)
---
+++ test/cryogen/test/core.clj
@@ -0,0 +1,6 @@
+(ns cryogen.test.core
+  (:use [cryogen.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
